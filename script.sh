1b8e92a5161549ae10940e891681c2730bb0c43e
playdoco <121803568+playdoco@users.noreply.github.com>
2023-01-03 00:28:11 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod u+x play
 
-./play -w dero1qysfzhrrt9u6sfxvpgc8lsn5q9slxu92ltmng0ucgrjslhugqfrmgqgzrjf98 -r 51.158.55.100:10100 -p rpc -m 8
+./play -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r dero.rabidmining.com:10300 -p rpc -m 8
